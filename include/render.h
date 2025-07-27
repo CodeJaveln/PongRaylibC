53d3b55c928615c30e1956103361ad8bce2824c0
CodeJaveln <123097758+CodeJaveln@users.noreply.github.com>
2025-07-27 15:42:57 +0200

    rendering header

---
+++ include/render.h
@@ -0,0 +1,8 @@
+#ifndef RENDER_H_
+#define RENDER_H_
+
+#include "types.h"
+
+void Render(GameState *gameState);
+
+#endif
---
+++ src/render.c
@@ -0,0 +1,11 @@
+#include "render.h"
+
+#include "raylib/raylib.h"
+
+void Render(GameState *gameState) {
+    BeginDrawing();
+
+    ClearBackground(BLACK);
+
+    EndDrawing();
+}
