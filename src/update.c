0ab4a8a358e1c1181a94b7eb1bb13c8657b1445e
LeadHood <adrianwallin07@gmail.com>
2025-07-27 16:14:04 +0200

    CLAMPING

--- src/update.c
+++ src/update.c
@@ -1,5 +1,7 @@
 #include "update.h"
 #include "raylib/raylib.h"
+#include "raylib/raymath.h"
+
 
 #define PLAYER_SPEED 5
 
@@ -14,9 +16,14 @@ void Update(GameState *gameState){
     if(IsKeyDown(KEY_S)){player1->velocity += PLAYER_SPEED;}
     player1->position.y += player1->velocity;
 
+    player1->position.y = Clamp(player1->position.y, 0, GetScreenHeight() - PLAYER_HEIGHT);
+
     //Player2 movement
     player2->velocity = 0;
     if(IsKeyDown(KEY_UP)){player2->velocity -= PLAYER_SPEED;}
     if(IsKeyDown(KEY_DOWN)){player2->velocity += PLAYER_SPEED;}
     player2->position.y += player2->velocity;
+
+    player2->position.y = Clamp(player2->position.y, 0, GetScreenHeight() - PLAYER_HEIGHT);
+    
 }
