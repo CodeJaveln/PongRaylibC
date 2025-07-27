1d6ed0a1e171d054928b2f41a9759dab611bf354
LeadHood <adrianwallin07@gmail.com>
2025-07-27 15:44:37 +0200

    Players movement

--- include/types.h
+++ include/types.h
@@ -1,10 +1,16 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include "raylib/raymath.h"
+
 typedef struct {
+    Vector2 position;
+    float velocity;
 } Player;
 
 typedef struct {
+    Vector2 position;
+    Vector2 velocity;
 } Ball;
 
 typedef struct {
